c2d2814d7626edde3a49e05496c48926f8f04988
Sofia Petsoulakis <petsofia@gmail.com>
2015-03-14 16:54:18 -0400

    started on the filter form, add a little css and added some more buttons to the nav bar

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,18 +1,52 @@
 class FiltersController < ApplicationController
-
+attr_reader :filter_results
 def new
    @user = current_user
+
+   @industries = Industry.all
+    @skills = Skill.all
+    @avails = Availability.all
+    @locations = Location.all
 
  end
 
  def create
 
+
+
  end
 
  def index
+   binding.pry
   @user = current_user
-
+  @filter_results
   @users = User.all
+
+
    
  end
+
+ def update
+    # binding.pry
+    filter_results(params)
+    industry = params["filter"]["industry_id"].to_i
+    avail = params["filter"]["availability_id"].to_i
+    skills = params["filter"]["skill_id"]
+    location = params["filter"]["job_location_id"]
+
+   @filter_results = User.where(availability_id: avail, industry_id: industry, skill_id: skills, job_location_id: location)
+
+     render :index
+ end
+
+ def mentors
+   binding.pry
+ end
+
+ def mentees
+   binding.pry
+
+ end
+
 end
+
