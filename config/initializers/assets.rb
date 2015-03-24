2a5e2fc2908b1e487aea111b452b8a2dd43d27f6
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-03-23 23:57:16 -0400

    added proper highlighting for pseudonav mentors mentees profile...had to precompile profile.css and changed from sass file to reg css and moved to list_stylesheets folder and unnested profile.css and disused @import typography tag

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( sss.js)
 Rails.application.config.assets.precompile += %w( mentorlist.css)
 Rails.application.config.assets.precompile += %w( menteelist.css)
+Rails.application.config.assets.precompile += %w( profile.css)
